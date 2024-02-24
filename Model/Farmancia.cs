c64f9dfc6373087bda0c407f39c73334cad12d4c
peteusom santana <petelsomsantana@gmail.com>
2024-02-24 23:39:30 +0100

    adicionado entidades

---
+++ Model/Farmancia.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_farmancias.Model
+{
+    public class Farmancia
+    {
+        public int Id {get;set;}
+        public string? Nome {get;set;}
+        public int N_de_telefone{get;set;}
+        public  DateTime HoraDeabertura{get;set;}
+        public  DateTime HoraDeEncerramento{get;set;}
+
+    }
+}
