eb27113a03121c9b318194ab87f589ea81e403cb
peteusom santana <petelsomsantana@gmail.com>
2024-02-26 15:07:30 +0100

    m

--- Model/Farmacia.cs
+++ Model/Farmacia.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Api_farmancias.Model
+namespace Api_farmacias.Model
 {
-    public class Farmancia
+    public class Farmacia
     {
         public int Id {get;set;}
         public string? Nome {get;set;}
