58ae65ed25976c8b41f30931af9fe8e8b9636087
lukenymanuel <lukenymanuel00@gmail.com>
2024-02-29 19:22:10 +0100

    DTO

---
+++ DTO/LocalizacaoDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_farmacias.Model;
+
+namespace Api_farmancias.Model
+{
+    
+    public class Localizacao
+    {
+        
+        public int Codigo_ip{get;set;}
+       
+
+
+    }
+}
---
+++ DTO/N_telefoneDTO.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_Farmacias.DTO
+{
+    
+    public class N_telefoneDTO
+    {
+       
+        public int telefone {get;set;}   
+    }
+}
