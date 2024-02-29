bad58836258abc9c785c03226dda3b727416bbbe
H3lboyAl3x <alexandreedgar77@gmai.com>
2024-02-29 21:23:48 +0100

    controllers, repository, interface

--- Controllers/FarmaciaControllers.cs
+++ Controllers/FarmaciaControllers.cs
@@ -18,11 +18,42 @@ namespace Api_Farmacias.Controllers
         {
             _farmfonte=farmfonte;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Farmacia>>> BuscartodasFarmacia()
+        {
+            List<Farmacia> farmacias = await _farmfonte.Farmancias();
+            return Ok(farmacias);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Farmacia>> Buscarfarmacia(int id)
+        {
+            Farmacia farmacias = await _farmfonte.BuscarFarmacia(id);
+            return Ok(farmacias);
+        }
+
         [HttpPost]
         [Route("adicionar")]
         public async Task<ActionResult<Farmacia>>Adicionarfarm([FromBody] Farmacia farmacia)
         {
+            Farmacia farmacias = await _farmfonte.AdicionarFarmacia(farmacia);
             return Ok(await _farmfonte.SaveAllAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Farmacia>> Atualiza([FromBody] Farmacia farmacia1, int id)
+        {
+            farmacia1.Id = id;
+            Farmacia farmacia = await _farmfonte.Atualizar(farmacia1, id);
+            return Ok(farmacia);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Farmacia>> Deletar(int id)
+        {
+            bool apagado = await _farmfonte.Apagar(id);
+            return Ok(apagado); 
         }
     }
 }
