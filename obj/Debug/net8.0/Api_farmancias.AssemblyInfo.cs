1ca92ddf686325b9634516991772667395e48142
peteusom santana <petelsomsantana@gmail.com>
2024-03-01 00:20:53 +0100

    Renomer Api_farmacias para Api_Farmacias

--- obj/Debug/net8.0/Api_farmancias.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api_farmancias.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Api_farmancias")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Api_Farmancias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Api_farmancias")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Api_farmancias")]
+[assembly: System.Reflection.AssemblyProductAttribute("Api_Farmancias")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Api_Farmancias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
