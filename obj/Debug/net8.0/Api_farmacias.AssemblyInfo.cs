7425c9ca5305cda548e4cdb16fd1438ecfa93b37
peteusom santana <petelsomsantana@gmail.com>
2024-03-01 11:32:37 +0100

    adicionando o AutoMaper

--- obj/Debug/net8.0/Api_farmacias.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api_farmacias.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Api_farmacias")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Api_Farmacias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca92ddf686325b9634516991772667395e48142")]
-[assembly: System.Reflection.AssemblyProductAttribute("Api_farmacias")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Api_farmacias")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Api_Farmacias")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Api_Farmacias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
