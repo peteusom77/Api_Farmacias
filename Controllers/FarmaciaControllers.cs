3bd5d60c9dab43216600838175caa598cd041938
peteusom santana <petelsomsantana@gmail.com>
2024-03-08 22:14:36 +0100

    correção

--- Controllers/FarmaciaControllers.cs
+++ Controllers/FarmaciaControllers.cs
@@ -33,12 +33,36 @@ namespace Api_Farmacias.Controllers
         }
 
         [HttpPost]
-        [Route("adicionar")]
-        public async Task<ActionResult<Farmacia>>Adicionarfarm([FromBody] Farmacia farmacia)
+        [Route("adicionarFarmacia")]
+        public async Task<ActionResult<Farmacia>>Adicionarfarm([FromBody] Todos todos)
         {
-            Farmacia farmacias = await _farmfonte.AdicionarFarmacia(farmacia);
-            return Ok(farmacias);
+            Farmacia farmacias = await _farmfonte.AdicionarFarmacia(todos.farmaciaDTO);
+            todos.localizacaoDTO.farmacia_id = farmacias.Id;
+            Localizacao localizacao = await _locali.AdicionarLocali(todos.localizacaoDTO);
+            return Ok(new { Farmacia = farmacias, Localizacao = localizacao });
         }
+        [HttpPost("AdicionarLocalizacao/{id_farm}")]
+        public async Task<ActionResult<Localizacao>> AdicionarLocalizacao([FromBody] LocalizacaoDTO locali, int id_farm)
+        {
+            try
+            {
+                var farmacia = await _farmfonte.BuscarFarmacia(id_farm);
+                if (farmacia == null)
+                {
+                    return NotFound($"Farmácia com ID {id_farm} não encontrada.");
+                }
+
+                locali.farmacia_id = farmacia.Id;
+                var localizacao = await _locali.AdicionarLocali(locali);
+
+                return Ok(localizacao);
+            }
+            catch (Exception ex)
+            {
+                 // Lide com a exceção de maneira apropriada, log ou retorne um erro HTTP 500, se necessário.
+                return StatusCode(500, $"Erro ao adicionar localização: {ex.Message}");
+            }
+}
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Farmacia>> Atualiza([FromBody] Farmacia farmacia1, int id)
