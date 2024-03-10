986a4a13ea505ff14efac1461c8cf3f876a52859
Hercules_Vanda <herhgrande@gmail.com>
2024-03-10 16:32:28 +0100

    direcao changes 2.0

--- Repositorio/DirecaoRepository.cs
+++ Repositorio/DirecaoRepository.cs
@@ -1,6 +1,9 @@
 using Api_Farmacias.Database;
 using Api_Farmacias.Model;
 using Api_Farmacias.Repositorio.Interface;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api_Farmacias.Repositorio
 {
     public class DirecaoRepository : IDirecaoRepository
@@ -19,7 +22,7 @@ namespace Api_Farmacias.Repositorio
         
             public Task<Direcao> AdicionsrDirecao(DirecaoDTO direcao)
             {
-                var farm = _mapper.Map<DirecaoDTO>(direcao);
+                var farm = _mapper.Map<Direcao>(direcao);
                 await _conexao.AddAsync(farm);
                 await _conexao.SaveChangesAsync();
                 return farm
@@ -27,7 +30,7 @@ namespace Api_Farmacias.Repositorio
 
             public Task<Direcao> AtualizarDirecao(DirecaoDTO direcao,int id)
             {
-                var farm = _mapper.Map<DirecaoDTO>(direcao);
+                var farm = _mapper.Map<Direcao>(direcao);
                 await _conexao.Update(farm);
                 await _conexao.SaveChangesAsync();
                 return farm;
