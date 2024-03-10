027d7bc957926525e957614b0050674abc033d69
lukenymanuel <lukenymanuel00@gmail.com>
2024-03-10 13:16:13 +0100

    Atualização do controller adicionar farmacias

--- DTO/DirecaoDTO.cs
+++ DTO/DirecaoDTO.cs
@@ -7,7 +7,7 @@ namespace Api_Farmancias.Model
         public string? Municipio{get;set;}
         public string? Rua{get;set;}
         public string? Ponto_de_referencia{get;set;}
-
+        public int farmacia_id{get;set;}
 
     }
 }
