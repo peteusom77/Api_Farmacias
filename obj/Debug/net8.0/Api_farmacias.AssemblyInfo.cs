3e454ca2c2188bd12b5c636059c21f50b0ca66df
H3lboyAl3x <alexandreedgar77@gmai.com>
2024-03-10 14:32:42 +0100

    commit 100

--- obj/Debug/net8.0/Api_farmacias.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api_farmacias.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Farmacias")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a663c8f005c1980137c9d9067dfe3ff51856746f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fe60dd0bda36be40e1a28185f715a9d8feac42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Farmacias")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Farmacias")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
